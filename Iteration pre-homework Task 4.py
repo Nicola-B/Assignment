15121cb8156d6da593c5d68115d2580bca99a041
Nicola-B <4030@longroad.ac.uk>
2014-12-04 21:53:04 +0100

    commit

---
+++ Iteration pre-homework Task 4.py
@@ -0,0 +1,9 @@
+#Nicola Batty
+#08/10/2014
+#Iteration pre-homework Task 4
+
+result = 0
+number = 1
+while not(number == 0):
+    number = int(input("Please input a number: "))
+    result = result + number
